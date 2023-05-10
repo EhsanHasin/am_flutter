94165c2ec57cc21b7957a5c2a317f3e63c7d99f4
EhsanHasin <EhsanHasin@gmail.com>
2023-05-10 11:53:14 +0430

    در جلسه امروز دهم می روی GridView و GestureDetector کار کردیم و همچنان یک اپ گالری ایجاد نمودیم

--- lib/grid_view.dart
+++ lib/grid_view.dart
@@ -15,6 +15,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var photos = [
+    'assets/images/photo_2019-03-15_22-39-23.jpg',
+    'assets/images/photo_2019-05-06_08-08-48.jpg',
+    'assets/images/photo_2020-03-12_20-10-36.jpg',
+    'assets/images/photo_2021-12-30_00-00-23.jpg',
+    'assets/images/photo_2023-01-29_20-10-40.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,23 +31,43 @@ class _HomePageState extends State<HomePage> {
       ),
       body: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 5, mainAxisSpacing: 3),
-          itemCount: 17,
+              crossAxisCount: 3, mainAxisSpacing: 3),
+          itemCount: photos.length,
           itemBuilder: (bc, index) {
             return GestureDetector(
               onTap: (){
-
+                Navigator.push(context, MaterialPageRoute(builder: (c)=>FullScreenPhoto(photo: photos[index],)));
               },
               child: GridTile(
                 child: Image.asset(
-                  "assets/images/pic.jpg",
+                  photos[index],
                   fit: BoxFit.cover,
                 ),
-                header: Icon(Icons.more_vert),
-                footer: Text("This is a footer"),
+                header: Icon(Icons.more_horiz),
+                footer: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(child: Text("Pic: 1200x800")),
+                ),
               ),
             );
           }),
     );
   }
 }
+
+class FullScreenPhoto extends StatelessWidget {
+
+  FullScreenPhoto({Key? key, this.photo}) : super(key: key);
+
+  var photo;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: (){
+          Navigator.pop(context);
+        },
+        child: Image(image: AssetImage(photo), width: double.infinity, height: double.infinity, fit: BoxFit.contain,));
+  }
+}
+
