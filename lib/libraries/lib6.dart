73774c11c5f430b624636e6fb907ad984dcd85b1
EhsanHasin <EhsanHasin@gmail.com>
2023-05-23 11:48:28 +0430

    Working on photo_view package.

---
+++ lib/libraries/lib6.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+void main() => runApp(
+    MaterialApp(
+      title: "e7 App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("e7 App"),),
+      body: Container(
+        child: PhotoView(imageProvider: AssetImage("assets/images/photo_2019-03-15_22-39-23.jpg")),
+      ),
+    );
+  }
+}
