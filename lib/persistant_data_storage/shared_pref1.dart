41c53aa09f7f6d449ebef2ea301f60d3ba4d8826
EhsanHasin <EhsanHasin@gmail.com>
2023-06-06 12:09:57 +0430

    Persistent Data Storage: working more on Share Preferences to store and restore data.

--- lib/persistant_data_storage/shared_pref1.dart
+++ lib/persistant_data_storage/shared_pref1.dart
@@ -22,9 +22,12 @@ final String AGE = "age";
 class _HomePageState extends State<HomePage> {
 
   late SharedPreferences preferences;
-
-  var firstName, lastName, age;
+  String firstName= "", lastName= "", age= "";
   var data = "";
+  TextEditingController tecFN = TextEditingController();
+  TextEditingController tecLN = TextEditingController();
+  TextEditingController tecA = TextEditingController();
+  String? eFN, eLN , eA ;
 
   @override
   Widget build(BuildContext context) {
@@ -43,44 +46,80 @@ class _HomePageState extends State<HomePage> {
                 style: TextStyle(fontSize: 30),
               ),
               TextField(
-                decoration: InputDecoration(hintText: "First Name"),
+                controller: tecFN,
+                decoration: InputDecoration(hintText: "First Name", errorText: eFN),
                 onChanged: (fn) {
                   firstName = fn;
+                  if(fn.isNotEmpty) eFN = null;
+                  setState(() {});
                 },
               ),
               TextField(
-                decoration: InputDecoration(hintText: "Last Name"),
+                controller: tecLN,
+                decoration: InputDecoration(hintText: "Last Name", errorText: eLN),
                 onChanged: (ln) {
                   lastName = ln;
+                  if(ln.isNotEmpty) eLN = null;
+                  setState(() {});
                 },
               ),
               TextField(
-                decoration: InputDecoration(hintText: "age"),
+                controller: tecA,
+                decoration: InputDecoration(hintText: "age", errorText: eA),
                 onChanged: (age) {
                   this.age = age;
+                  if(age.isNotEmpty) eA = null;
+                  setState(() {});
                 },
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(onPressed: () async{
+                child: ElevatedButton(
+                    onPressed: () async{
+                      if(tecFN.text.isEmpty){
+                        setState(() {
+                          eFN = "Enter your first name";
+                        });
+                        return;
+                      }
 
-                  preferences = await SharedPreferences.getInstance();
-                  await preferences.setString(FIRST_NAME, firstName);
-                  await preferences.setString(LAST_NAME, lastName);
-                  await preferences.setString(AGE, age);
+                      if(tecLN.text.isEmpty){
+                        setState(() {
+                          eLN = "Enter your last name";
+                        });
+                        return;
+                      }
 
-                }, child: Text("Save")),
+                      if(tecA.text.isEmpty){
+                        setState(() {
+                          eA = "Enter your age";
+                        });
+                        return;
+                      }
+
+                      preferences = await SharedPreferences.getInstance();
+                      await preferences.setString(FIRST_NAME, firstName);
+                      await preferences.setString(LAST_NAME, lastName);
+                      await preferences.setString(AGE, age);
+                      tecFN.clear();
+                      tecLN.clear();
+                      tecA.clear();
+                      setState(() {});
+                },
+                    child: Text("Save")),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(onPressed: () async{
+                child: ElevatedButton(
+                    onPressed: () async{
                   preferences = await SharedPreferences.getInstance();
-                  firstName = await preferences.getString(FIRST_NAME) ?? "no name";
-                  lastName = await preferences.getString(LAST_NAME) ?? "no last name";
-                  age = await preferences.getString(AGE) ?? "no age";
+                  firstName = preferences.getString(FIRST_NAME) ?? "no name";
+                  lastName = preferences.getString(LAST_NAME) ?? "no last name";
+                  age = preferences.getString(AGE) ?? "no age";
                   data = "you are $firstName $lastName and your age is $age years old";
                   setState(() {});
-                }, child: Text("Restore")),
+                },
+                    child: Text("Restore")),
               ),
               Text(
                 "Data = $data",
@@ -88,9 +127,19 @@ class _HomePageState extends State<HomePage> {
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(onPressed: (){
-
-                }, child: Text("Delete")),
+                child: ElevatedButton(
+                    onPressed: () async{
+                      preferences = await SharedPreferences.getInstance();
+                      var bfn = await preferences.remove(FIRST_NAME);
+                      var bln = await preferences.remove(LAST_NAME);
+                      var ba = await preferences.remove(AGE);
+                      if(bfn && bln && ba){
+                        setState(() {
+                          data = "";
+                        });
+                      }
+                    },
+                    child: Text("Delete")),
               ),
             ],
           ),
